247c3f8a1ec04d648c9d7eada9517d3f70d90f16
Dorikyh🍃 <80162941+Dorikyh@users.noreply.github.com>
2024-09-13 16:55:16 -0500

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,4 @@
 import { z, defineCollection } from "astro:content";
-const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
-});
 
 const charlaSchema = z.object({
     title: z.string(),
@@ -20,13 +9,10 @@ const charlaSchema = z.object({
     link: z.string().optional(),
 });
 
-export type BlogSchema = z.infer<typeof blogSchema>;
-export type charlaSchema = z.infer<typeof charlaSchema>;
+export type CharlaSchema = z.infer<typeof charlaSchema>;
 
-const blogCollection = defineCollection({ schema: blogSchema });
 const charlaCollection = defineCollection({ schema: charlaSchema });
 
 export const collections = {
-    'blog': blogCollection,
-    'charla': charlaCollection
-}
+    'charla': charlaCollection,
+};
